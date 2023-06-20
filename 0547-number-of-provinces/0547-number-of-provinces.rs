20ccc52d7bca643727a68a38507a9e4eda89ee6f
dragonFly0180 <130906662+dragonFly0180@users.noreply.github.com>
2023-06-20 12:55:38 +0530

    Time: 4 ms (81.38%), Space: 2.4 MB (19.31%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.rs
+++ 0547-number-of-provinces/0547-number-of-provinces.rs
@@ -1,32 +1,4 @@
 // dfs
-// impl Solution {
-//     pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
-//         let mut cnt: i32 = 0;
-//         let mut vis: Vec<bool> = vec![false; is_connected.len()];
-
-//         for node in 0..is_connected.len() {
-//             if !vis[node] {
-//                 cnt += 1;
-//                 Self::dfs(&is_connected, &mut vis, node);
-//             }
-//         }
-//         cnt
-//     }
-
-//     fn dfs(is_connected: &[Vec<i32>], vis: &mut [bool], node: usize) {
-//         vis[node] = true;
-
-//         for adj_node in 0..is_connected.len() {
-//             if is_connected[node][adj_node] == 1 && !vis[adj_node] {
-//                 Self::dfs(is_connected, vis, adj_node);
-//             }
-//         }
-//     }
-// }
-
-// bfs
-use std::collections::VecDeque;
-
 impl Solution {
     pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
         let mut cnt: i32 = 0;
@@ -35,29 +7,57 @@ impl Solution {
         for node in 0..is_connected.len() {
             if !vis[node] {
                 cnt += 1;
-                Self::bfs(&is_connected, &mut vis, node);
+                Self::dfs(&is_connected, &mut vis, node);
             }
         }
         cnt
     }
 
-    fn bfs(is_connected: &[Vec<i32>], vis: &mut [bool], node: usize) {
-        let mut q: VecDeque<usize> = VecDeque::new();
-        q.push_back(node);
+    fn dfs(is_connected: &[Vec<i32>], vis: &mut [bool], node: usize) {
         vis[node] = true;
-        
-        while !q.is_empty() {
-            let node: usize = q.pop_front().unwrap();
-            
-            for adj_node in 0..is_connected.len() {
-                if is_connected[node][adj_node] == 1 && !vis[adj_node] {
-                    q.push_back(adj_node);
-                    vis[adj_node] = true;
-                }
+
+        for adj_node in 0..is_connected.len() {
+            if is_connected[node][adj_node] == 1 && !vis[adj_node] {
+                Self::dfs(is_connected, vis, adj_node);
             }
         }
     }
 }
+
+// // bfs
+// use std::collections::VecDeque;
+
+// impl Solution {
+//     pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+//         let mut cnt: i32 = 0;
+//         let mut vis: Vec<bool> = vec![false; is_connected.len()];
+
+//         for node in 0..is_connected.len() {
+//             if !vis[node] {
+//                 cnt += 1;
+//                 Self::bfs(&is_connected, &mut vis, node);
+//             }
+//         }
+//         cnt
+//     }
+
+//     fn bfs(is_connected: &[Vec<i32>], vis: &mut [bool], node: usize) {
+//         let mut q: VecDeque<usize> = VecDeque::new();
+//         q.push_back(node);
+//         vis[node] = true;
+        
+//         while !q.is_empty() {
+//             let node: usize = q.pop_front().unwrap();
+            
+//             for adj_node in 0..is_connected.len() {
+//                 if is_connected[node][adj_node] == 1 && !vis[adj_node] {
+//                     q.push_back(adj_node);
+//                     vis[adj_node] = true;
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 
